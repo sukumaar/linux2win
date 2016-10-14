7efdf448ff138395de4845898edcb642ede25c4a
sukumaar <sukumaar.neo@gmail.com>
2016-10-14 12:59:40 +0530

    linux command in windows

---
+++ gtfo.bat
@@ -0,0 +1,10 @@
+@echo off
+echo  "========================"
+echo "|                        |"
+echo "|                        |"
+echo "|      OK Fine, Bye      |"
+echo "|                        |"
+echo "|                        |"
+echo  "========================" 
+timeout 1
+exit
---
+++ ip.bat
@@ -0,0 +1,2 @@
+@echo off
+java -jar D:\Sukumaar\myCode\IPFromScala\target\scala-2.10\ipfromscala-assembly-0.1-SNAPSHOT.jar
